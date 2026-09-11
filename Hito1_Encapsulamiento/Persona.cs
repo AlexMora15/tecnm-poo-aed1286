0548e449df5799e660b21c1668710f3174a4f620
AlexMora15 <tareastecnmalex@gmail.com>
2026-09-11 10:22:40 -0500

    Implement private attributes and validation properties

--- Hito1_Encapsulamiento/Persona.cs
+++ Hito1_Encapsulamiento/Persona.cs
@@ -2,6 +2,41 @@ namespace Hito1_Encapsulamiento;
 
 public class Persona
 {
-    // TODO: Implementar atributos privados, propiedades con validacion,
-    // constructor con defensa de invariantes y metodo CumplirAnios().
+    private string nombre;
+    private int edad;
+
+    public string Nombre
+    {
+        get => nombre;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("El nombre no puede estar vacio.");
+
+            nombre = value;
+        }
+    }
+
+    public int Edad
+    {
+        get => edad;
+        private set
+        {
+            if (value < 0)
+                throw new ArgumentException("La edad no puede ser negativa.");
+
+            edad = value;
+        }
+    }
+
+    public Persona(string nombre, int edad)
+    {
+        Nombre = nombre;
+        Edad = edad;
+    }
+
+    public void CumplirAnios()
+    {
+        Edad++;
+    }
 }
